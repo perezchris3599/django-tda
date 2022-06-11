401b1a71f68ba49fcfa67da6b329c2ce6c2fee83
perezchris3599 <kweeziperez712@gmail.com>
2022-06-11 14:50:14 +0300

    made database migrations and registered model in admin

--- todoProject/todo/models.py
+++ todoProject/todo/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class ToDo(models.Model):
-    title = models.Charfield(max_length=80)
+    title = models.CharField(max_length=80)
     complete = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
